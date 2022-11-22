47ca0fc6ce8d1224c7e521311a59e5160fbdb057
Danshaning <danshaning@gmail.com>
2022-11-22 10:37:25 -0600

    Add project files.

---
+++ PandemicDiary_ShaningA/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace PandemicDiary_ShaningA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.capt = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.customizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(59, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 372);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_back
+            // 
+            this.btn_back.Location = new System.Drawing.Point(84, 491);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(101, 42);
+            this.btn_back.TabIndex = 2;
+            this.btn_back.Text = "<<";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(299, 491);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(104, 41);
+            this.btn_next.TabIndex = 3;
+            this.btn_next.Text = ">>";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // capt
+            // 
+            this.capt.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.capt.Location = new System.Drawing.Point(527, 51);
+            this.capt.Name = "capt";
+            this.capt.Size = new System.Drawing.Size(313, 372);
+            this.capt.TabIndex = 4;
+            this.capt.Text = "label1";
+            this.capt.Click += new System.EventHandler(this.capt_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.customizeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(874, 30);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // customizeToolStripMenuItem
+            // 
+            this.customizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
+            this.customizeToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.customizeToolStripMenuItem.Text = "Customize";
+            // 
+            // backgroundToolStripMenuItem
+            // 
+            this.backgroundToolStripMenuItem.Name = "backgroundToolStripMenuItem";
+            this.backgroundToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.backgroundToolStripMenuItem.Text = "Background Color";
+            this.backgroundToolStripMenuItem.Click += new System.EventHandler(this.backgroundToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(874, 558);
+            this.Controls.Add(this.capt);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Label capt;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ PandemicDiary_ShaningA/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PandemicDiary_ShaningA
+{
+    public partial class Form1 : Form
+    {
+        Image[] pics = new Image[5];
+
+        string[] captions = new string[5];
+
+        int currentIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //populate the pics array
+                pics[0] = Image.FromFile("firstPic.jpg");
+                pics[1] = Image.FromFile("secPic.jpg");
+                pics[2] = Image.FromFile("thirdPic.jpg");
+                pics[3] = Image.FromFile("fourthpic.jpg");
+                pics[4] = Image.FromFile("fifthPic.jpg");
+
+                //populate the captions array
+                captions[0] = "Covid19 is a crazy story, and I believe it learns us all a big lesson. Everything can change from one day to the other in this global world. I mean who could ever imagine that the whole world will be mandated to wear a mask in public space. This just shows us that even the smallest thing like breathing freely is not granted.";
+                captions[1] = "Bringing back the focus on me, I can say that I was very affected by this situation. In fact, I came to the U.S at the beginning of 2020, just before covid started and the lockdown was first emitted. This situation has been a very weird experience since I did not know many people and was trying to adapt to the new culture. I don't need to say that it was not possible. The lockdown has caused the classes to go online, which highly limited interaction with people. And with no family around, I can tell you enough about how lonely I felt during the first months. ";
+                captions[2] = "During that period, I browsed a lot on the internet. From dating apps like tinder to streaming apps like Netflix. In fact, I think I watched more movies and shows during 2020-2021 than I did in the last ten years of my life. Seriously! Netflix and my bed were my refuges. ";
+                captions[3] = "Food delivery company increased their market share from under 20% in 2018 to 53% in 2021. Yes, like most of us, 2021 was not a good year in terms of good alimentation habits. I believe I highly participated in the Uber eats market share spike. I was giving myself excuses to not cook at home and eat all the junk food possible near me, without putting one foot outside. Meanwhile, my weight spiked too ...";
+                captions[4] = "Now that life is getting back to normal, I think I have to take back control of my health wellbeing. In-person classes started to be offered again and my objective is to lose those extra pounds quickly to show the best version of myself. Also, I deleted a delivery app because the temptation is just insane. \n\nGood luck to everyone going back out there! See you soon hopefully.";
+
+                pictureBox1.Image = pics[currentIndex];
+
+                capt.Text = captions[currentIndex];
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("The file " + ex.Message + " does not exist.");
+            }
+
+        }
+
+        private void capt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currentIndex--; //increment currentIndex
+
+
+                //refresh picturebox
+                pictureBox1.Image = pics[currentIndex];
+                capt.Text = captions[currentIndex];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("You've reached the first picture.", "Last Picture");
+                currentIndex++;
+
+            }
+
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currentIndex++; //increment currentIndex
+
+
+                //refresh picturebox
+                pictureBox1.Image = pics[currentIndex];
+                capt.Text = captions[currentIndex];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("You've reached the last picture.", "Last Picture");
+                //MessageBox.Show(ex.Message);
+                currentIndex--;
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult ans = MessageBox.Show("Are you sure you want to quit?",
+                                               "QUIT?",
+                                               MessageBoxButtons.YesNo);
+
+            if (ans == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void backgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            capt.Font = fontDialog1.Font;
+        }
+    }
+}
